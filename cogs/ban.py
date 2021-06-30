6c54f4f4465ebdcbe8e9d6b00e3b8d6635306aaa
Bram1903 <Bram-Dekker@live.nl>
2021-06-30 09:01:07 +0200

    Changed thumbnail

--- cogs/ban.py
+++ cogs/ban.py
@@ -9,16 +9,16 @@ class Ban(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Ban module has succesfully been loaded.')
+        print('Ban module has succesfully been initialized.')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason)
         embedban = Embed(title="CartelPvP | Moderation",
-                        description="You have been banned from CartelPvP.",
+                        description=f"{member} has been banned from CartelPvP.",
                         colour=0xAE0808)
-        embedban.set_thumbnail(url=ctx.author.avatar_url)
+        embedban.set_thumbnail(url="https://cdn.discordapp.com/attachments/807568994202025996/854995835154202644/lg-1.png")
         embedban.add_field(name="Banned by", value=f"{ctx.author}", inline=True)
         embedban.add_field(name="Reason", value=f"{reason}", inline=True)
         await ctx.send(embed=embedban)
--- cogs/kick.py
+++ cogs/kick.py
@@ -9,16 +9,16 @@ class Kick(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Kick module has succesfully been loaded.')
+        print('Kick module has succesfully been initialized.')
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member : discord.Member, *, reason="Not specified"):
         await member.kick(reason=reason)
         embedkick = Embed(title="CartelPvP | Moderation",
-                        description="You have been kicked from CartelPvP.",
+                        description=f"{member} has been kicked from CartelPvP.",
                         colour=0xAE0808)
-        embedkick.set_thumbnail(url=ctx.author.avatar_url)
+        embedkick.set_thumbnail(url="https://cdn.discordapp.com/attachments/807568994202025996/854995835154202644/lg-1.png")
         embedkick.add_field(name="Kicked by", value=f"{ctx.author}", inline=True)
         embedkick.add_field(name="Reason", value=f"{reason}", inline=True)
         await ctx.send(embed=embedkick)
