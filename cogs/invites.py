379d88837b08764e7f9cf8757f75617fa824843d
Bram1903 <Bram-Dekker@live.nl>
2021-06-30 11:35:59 +0200

    Added invites.py

---
+++ cogs/invites.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+
+class Invites(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Invite module has successfully been initialized.')
+
+    @commands.command()
+    async def invites(self, ctx, user:
+    discord.Member = None):
+        if user is None:
+            total_invites = 0
+            for i in await ctx.guild.invites():
+                if i.inviter == ctx.author:
+                    total_invites += i.uses
+            await ctx.send(f"You've invited {total_invites} member{'' if total_invites == 1 else 's'} to the server!")
+        else:
+            total_invites = 0
+            for i in await ctx.guild.invites():
+                if i.inviter == user:
+                    total_invites += i.uses
+
+            await ctx.send(
+                f"{user} has invited {total_invites} member{'' if total_invites == 1 else 's'} to the server!")
+
+
+def setup(client):
+    client.add_cog(Invites(client))
