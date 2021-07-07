2931d3f7d08835af0148e66c41cd07f444428e0d
Bram1903 <Bram-Dekker@live.nl>
2021-07-07 12:03:31 +0200

    Fixed link

--- cogs/link.py
+++ cogs/link.py
@@ -1,4 +1,5 @@
 import discord
+import requests
 from discord.ext import commands
 from discord import Embed
 import json
@@ -6,7 +7,7 @@ import json
 with open("./config.json") as configFile:
     data = json.load(configFile)
     for value in data["server_details"]:
-        test = 'test'
+        configured_ip = value['configured_ip']
 
 
 class link(commands.Cog):
@@ -19,7 +20,21 @@ class link(commands.Cog):
 
     @commands.command()
     async def link(self, ctx, *, token):
-        await ctx.send("test")
+        payload = {'token': token, 'userId': ctx.author}
+        r = requests.post(configured_ip, data=payload)
+
+    @commands.command()
+    async def test(self, ctx, *, token):
+        payload = {'token': token, 'userId': ctx.author}
+        Status = Embed(title="CartelPvP | Linking",
+                       description=f"Trying to link you accounts",
+                       colour=0xAE0808)
+
+        Status.set_thumbnail(
+            url="https://cdn.discordapp.com/attachments/807568994202025996/854995835154202644/lg-1.png")
+        Status.add_field(name="User", value=f"{ctx.author}", inline=True)
+        Status.add_field(name="Token", value=f"{token}", inline=True)
+        await ctx.send(embed=Status)
 
 
 def setup(client):
