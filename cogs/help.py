b89ae5081f6771ea8ed3cf717cfd6002ca286e51
Bram <Bram-Dekker@live.nl>
2021-07-17 23:56:23 +0200

    Added first help command

--- cogs/help.py
+++ cogs/help.py
@@ -14,7 +14,13 @@ class Invites(commands.Cog):
 
     @commands.group(name='help', invoke_without_command=True)
     async def help(self, ctx):
-        await ctx.send("Base command")
+        help = Embed(title="CartelPvP | Help",
+                     colour=0xAE0808)
+        help.set_thumbnail(url="https://cdn.discordapp.com/attachments/807568994202025996/854995835154202644/lg-1.png")
+        help.add_field(name="Sub commands", value="```Info           | ?help info\n"
+                                                  "Moderation     | ?help Moderation\n"
+                                                  "Administration | ?help Administration```")
+        await ctx.send(embed=help)
 
     @help.command(name='info')
     async def info_subcommand(self, ctx):
