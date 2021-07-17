1364e7f41586ca13ffacbb505ef057e61a49fcdb
Bram <Bram-Dekker@live.nl>
2021-07-17 08:26:25 +0200

    yes

--- cogs/echo.py
+++ cogs/echo.py
@@ -18,6 +18,7 @@ class Echo(commands.Cog):
         if not sentence:
             return await ctx.send("You must provide a word or sentence.")
         await ctx.send(await commands.clean_content().convert(ctx=ctx, argument=sentence))
+        await ctx.message.delete()
 
 
 def setup(client):
