396a714a538bab1e3d22c8ca14eefe9d53325a69
Bram <Bram-Dekker@live.nl>
2021-07-21 19:42:37 +0200

    fucking stupid fucking stupid bitch intents fuck you

--- bot.py
+++ bot.py
@@ -33,14 +33,12 @@ else:
             TICKET_CATEGORY_ID = value['ticket_category_id']
             TICKET_CHANNEL_ID = value['ticket_channel_id']
 
-intents = discord.Intents(members=True)
 client = Bot(command_prefix="?",
              help_command=None,
              case_insensitive=True,
              max_messages=100,
              activity=Activity(type=ActivityType.watching,
-                               name=f"over Cartel."),
-             intents=intents)
+                               name=f"over Cartel."))
 
 
 @client.group(name='system', invoke_without_command=True)
