d8eba4def94d35a1572cc3fa02efb84ee0c053f2
Bram <Bram-Dekker@live.nl>
2021-07-30 14:26:35 +0200

    Optimized fileblocker

--- cogs/fileblocker.py
+++ cogs/fileblocker.py
@@ -19,12 +19,12 @@ class fileblocker(commands.Cog):
         permission = message.author.guild_permissions.manage_messages
         if not permission:
             for file in message.attachments:
-                if file.filename.endswith((".exe", ".dll")):
+                if file.filename.endswith((".exe", ".bat", ".vb", ".jar")):
                     await message.delete()
-                    NoPerm = Embed(colour=0xAE0808)
-                    NoPerm.set_author(name='You are not allowed to send that file type.',
+                    no_perm = Embed(colour=0xAE0808)
+                    no_perm.set_author(name='You are not allowed to send that file type.',
                                       icon_url='https://i.imgur.com/uoq4zFS.png')
-                    msg = await message.channel.send(embed=NoPerm)
+                    msg = await message.channel.send(embed=no_perm)
                     await sleep(4.7)
                     await msg.delete()
         else:
