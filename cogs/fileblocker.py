5e0b0acab7902b8b9777cf713a7728c3780d7a62
Bram1903 <Bram-Dekker@live.nl>
2021-08-01 01:22:43 +0200

    Update fileblocker.py

--- cogs/fileblocker.py
+++ cogs/fileblocker.py
@@ -1,7 +1,17 @@
+import datetime
+import json
+import os
 from asyncio import sleep
 
+import discord
 from discord import Embed
 from discord.ext import commands
+
+with open("./config.json") as configFile:  # Opens the file config.json as a config file
+    data = json.load(configFile)  # Var data is the value in the json.config file
+    for value in data["server_details"]:  # For the data in server_details
+        logging_channel = value['logging_channel']  # Gets the specific data
+        admins = value['admins']
 
 
 class fileblocker(commands.Cog):
@@ -22,11 +32,28 @@ class fileblocker(commands.Cog):
             if not permission:
                 for file in message.attachments:
                     if file.filename.endswith((".exe", ".bat", ".vb", ".jar")):
-                        await message.delete()
+                        for attachment in message.attachments:
+                            await attachment.save(attachment.filename)
                         no_perm = Embed(colour=0xAE0808)
                         no_perm.set_author(name='You are not allowed to send that file type.',
                                            icon_url='https://i.imgur.com/uoq4zFS.png')
                         msg = await message.channel.send(embed=no_perm)
+                        timestamp = datetime.datetime.utcnow()
+                        embed_logs = Embed(description=f"User ID: {message.author.id}", colour=0xE67E22)
+                        embed_logs.set_author(name='File Removed',
+                                              icon_url='https://i.imgur.com/uoq4zFS.png'),
+                        embed_logs.add_field(name="User", value=f"{message.author.mention}",
+                                             inline=False)
+                        embed_logs.add_field(name="Channel", value=f"{message.channel.mention}",
+                                             inline=False)
+                        embed_logs.set_footer(text=f"Removed on {timestamp}"
+                                              , icon_url=message.author.avatar_url)
+                        await message.delete()
+                        logs = self.client.get_channel(int(logging_channel))
+                        await logs.send(embed=embed_logs)
+                        file = discord.File(attachment.filename)
+                        await logs.send(file=file)
+                        os.remove(attachment.filename)
                         await sleep(4.7)
                         await msg.delete()
             else:
