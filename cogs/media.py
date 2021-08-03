79678035f02bac900b70d011fd2e91db12b8b932
Bram1903 <Bram-Dekker@live.nl>
2021-08-03 14:11:27 +0200

    Added media grant/revoke

---
+++ cogs/media.py
@@ -0,0 +1,145 @@
+import datetime
+import json
+
+import discord
+from discord import Embed
+from discord.ext import commands
+from asyncio import sleep
+from discord.utils import get
+
+with open("./config.json") as configFile:  # Opens the file config.json as a config file
+    data = json.load(configFile)  # Var data is the value in the json.config file
+    for value in data["server_details"]:  # For the data in server_details
+        role_media_admin = value['media-admin']
+        role_youtuber = value['youtuber']
+        role_famous = value['famous']
+        logging_channel = value['logging_channel']  # Gets the specific data
+        admins = value['admins']
+
+
+class Media(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Media has successfully been initialized.')
+
+    @commands.command(pass_context=True)
+    async def grant(self, ctx, member: discord.Member=None, rank=None):
+        media_role_id = (int(role_media_admin))
+        role = get(ctx.guild.roles, id=media_role_id)
+        if role in ctx.author.roles:
+            if not member:
+                msg = await ctx.send('You must provide a user.')
+                await sleep(4.7)
+                await msg.delete()
+                return
+            if not rank:
+                msg = await ctx.send('You must provide a rank.')
+                await sleep(4.7)
+                await msg.delete()
+                return
+            if rank.lower() == "youtuber":
+                youtuber_id = (int(role_youtuber))
+                youtuber = get(ctx.guild.roles, id=youtuber_id)
+                await member.add_roles(youtuber)
+                nick = f"YouTuber | {member.display_name}"
+                await member.edit(nick=nick)
+                embed = Embed(colour=0x57F287)
+                embed.set_author(name=f'Granted YouTuber to {member.display_name}.',
+                            icon_url='https://i.imgur.com/0Lzd0go.png')
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+            elif rank.lower() == "famous":
+                famous_id = (int(role_famous))
+                famous = get(ctx.guild.roles, id=famous_id)
+                await member.add_roles(famous)
+                nick = f"Famous | {member.display_name}"
+                await member.edit(nick=nick)
+                embed = Embed(colour=0x57F287)
+                embed.set_author(name=f'Granted Famous to {member.display_name}.',
+                                 icon_url='https://i.imgur.com/0Lzd0go.png')
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+            else:
+                embed = Embed(colour=0xAE0808)
+                embed.set_author(name='Invalid Rank',
+                                 icon_url='https://i.imgur.com/SR9wWm9.png')
+                embed.add_field(name="Options", value="```\nyoutuber\nfamous```",
+                                inline=False)
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+        else:
+            await ctx.message.delete()
+            no_perm = Embed(colour=0xAE0808)
+            no_perm.set_author(name='You are not an media administrator.',
+                               icon_url='https://i.imgur.com/sFhjp83.png')
+            msg = await ctx.channel.send(embed=no_perm)
+            await sleep(4.7)
+            await msg.delete()
+
+    @commands.command(pass_context=True)
+    async def revoke(self, ctx, member: discord.Member = None, rank=None):
+        media_role_id = (int(role_media_admin))
+        role = get(ctx.guild.roles, id=media_role_id)
+        if role in ctx.author.roles:
+            if not member:
+                msg = await ctx.send('You must provide a user.')
+                await sleep(4.7)
+                await msg.delete()
+                return
+            if not rank:
+                msg = await ctx.send('You must provide a rank.')
+                await sleep(4.7)
+                await msg.delete()
+                return
+            if rank.lower() == "youtuber":
+                youtuber_id = (int(role_youtuber))
+                youtuber = get(ctx.guild.roles, id=youtuber_id)
+                await member.remove_roles(youtuber)
+                nick = member.name
+                await member.edit(nick=nick)
+                embed = Embed(colour=0xAE0808)
+                embed.set_author(name=f'Revoked YouTuber from {member.display_name}.',
+                                 icon_url='https://i.imgur.com/0Lzd0go.png')
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+            elif rank.lower() == "famous":
+                famous_id = (int(role_famous))
+                famous = get(ctx.guild.roles, id=famous_id)
+                await member.remove_roles(famous)
+                nick = member.name
+                await member.edit(nick=nick)
+                embed = Embed(colour=0xAE0808)
+                embed.set_author(name=f'Revoked Famous from {member.display_name}.',
+                                 icon_url='https://i.imgur.com/0Lzd0go.png')
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+            else:
+                embed = Embed(colour=0xAE0808)
+                embed.set_author(name='Invalid Rank',
+                                 icon_url='https://i.imgur.com/SR9wWm9.png')
+                embed.add_field(name="Options", value="```\nyoutuber\nfamous```",
+                                inline=False)
+                msg = await ctx.send(embed=embed)
+                await sleep(4.7)
+                await msg.delete()
+        else:
+            await ctx.message.delete()
+            no_perm = Embed(colour=0xAE0808)
+            no_perm.set_author(name='You are not an media administrator.',
+                               icon_url='https://i.imgur.com/sFhjp83.png')
+            msg = await ctx.channel.send(embed=no_perm)
+            await sleep(4.7)
+            await msg.delete()
+
+
+def setup(client):
+    client.add_cog(Media(client))
