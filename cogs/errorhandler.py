cdf90b2bfcdab0ca2850de9c36c3cda343fcce7c
Bram <Bram-Dekker@live.nl>
2021-08-04 12:54:40 +0200

    test

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -31,8 +31,7 @@ class CommandErrorHandler(commands.Cog):
         if isinstance(error, ignored):
             return
         else:
-            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            pass
         if isinstance(error, commands.MissingPermissions):
             # generic error handler for commands from guilds without permissions
             perms = str(error.missing_perms)[1:][:-1].title().replace("_", " ").replace("'", "")
